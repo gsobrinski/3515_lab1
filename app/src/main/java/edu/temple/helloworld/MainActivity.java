ca0149e52a397637e8c3b1fee8bc042078e2d18c
gsobrinski <gsobrinski99@gmail.com>
2021-02-04 13:18:15 -0500

    added on click event to the button

--- app/src/main/java/edu/temple/helloworld/MainActivity.java
+++ app/src/main/java/edu/temple/helloworld/MainActivity.java
@@ -3,12 +3,29 @@ package edu.temple.helloworld;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // get element by ID
+        textView = findViewById(R.id.textView);
+        //textView.setText("click the button!");
+
+        // single use onclick listener - created using a Java anonymous class (nameless class)
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                textView.setText("you clicked the button!");
+            }
+        });
     }
 }
